d997479af36e28ca5c260f3022aeb04105f6da38
Serhii Tkachenko <doker.tk@gmail.com>
2022-11-03 01:21:05 +0100

    Created NavigationController and MainViewController

--- Marvel/Sources/App/SceneDelegate.swift
+++ Marvel/Sources/App/SceneDelegate.swift
@@ -14,8 +14,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let windowScene = (scene as? UIWindowScene) else { return }
 		window = UIWindow(windowScene: windowScene)
-		window?.rootViewController = ViewController()
 		window?.makeKeyAndVisible()
+		
+		let mainViewController = MainViewController()
+		let navigationController = UINavigationController(rootViewController: mainViewController)
+		window?.rootViewController = navigationController
 	}
 
 }
--- Marvel/Sources/ViewControllers/CharactersViewController.swift
+++ Marvel/Sources/ViewControllers/CharactersViewController.swift
@@ -8,18 +8,18 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController {
+class CharactersViewController: UIViewController {
 	
 	let testURL = "https://gateway.marvel.com/v1/public/characters?nameStartsWith=Spider&orderBy=-modified&ts=Serhii-Tkachenko&apikey=d8182c561967ebc637775965e3484849&hash=c4fb21a13465834c8d7d8d816f45c16a"
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = .red
+		view.backgroundColor = .white
 		fetchCharacter()
 	}
 }
 
-extension ViewController {
+extension CharactersViewController {
 	
 	func fetchCharacter() {
 		let request = AF.request(testURL)
